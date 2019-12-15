39411c9264211cb786291ad7612b5c7b99a99605
billiamchoi <billiamchoi4u@gmail.com>
2019-12-15 15:06:25 -0800

    add travel register page and success

--- routes/travelings.js
+++ routes/travelings.js
@@ -28,4 +28,19 @@ router.get('/new', (req, res, next) => {
   res.render('travelings/new', {messages: req.flash()});
 });
 
+router.post('/', needAuth, catchErrors(async (req, res, next) => {
+  const user = req.user;
+  var traveling = new Traveling({
+    name: req.body.name,
+    location: req.body.location,
+    price: req.body.price,
+    description: req.body.description,
+    course: req.body.course,
+    author: user._id 
+  });
+  await traveling.save();
+  req.flash('success', 'Successfully posted');
+  res.redirect('/travelings');
+}));
+
 module.exports = router;
