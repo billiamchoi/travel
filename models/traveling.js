f501a56cbda75b7733cb6ce36b01dc77130cc9a5
billiamchoi <billiamchoi4u@gmail.com>
2020-07-17 15:21:36 +0900

    디렉토리 이름 모두 복수로 통일

--- models/traveling.js
+++ models/traveling.js
@@ -10,10 +10,6 @@ var schema = new Schema({
   description: {type: String, trim: true, required: true},
   course: {type: String, trim: true, required: true},
   guide: {type: String, trim: true, required: true},
-  numLikes: {type: Number, default: 0},
-  numDisLikes: {type: Number, default: 0},
-  numReservation: {type: Number, default: 0},
-  numReads: {type: Number, default: 0},
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
